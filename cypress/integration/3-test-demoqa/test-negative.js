967889255022a50d1cb6ed88c6d905536cb0d0d5
Fadlian17 <alfansyahfadlian@gmail.com>
2021-11-27 12:39:13 +0700

    first commit qa

---
+++ cypress/integration/3-test-demoqa/test-negative.js
@@ -0,0 +1,15 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+
+///open link website tidak sesuai
+
+
+describe('My First Cypress Test', function() {
+    it('Visits the ToolsQA Demo Page and check the menu items', function() {
+    //Visit the Demo QA Website
+    cy.visit("https://demoqalasd.com/");
+
+        
+})
+})
